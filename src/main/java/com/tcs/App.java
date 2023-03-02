61c64144b761fef2716ed0f75c66f5a26b0af015
soujanya34 <kapasoujanya1234@gmail.com>
2023-03-02 08:24:45 +0530

    Added set method in app.java

--- src/main/java/com/tcs/App.java
+++ src/main/java/com/tcs/App.java
@@ -6,8 +6,13 @@ package com.tcs;
  */
 public class App 
 {
+    String str=null;
     public static void main( String[] args )
     {
         System.out.println( "Hello- This is app.java" );
+    }
+
+    public void setMessage(String msg){
+        this.msg=msg;
     }
 }
